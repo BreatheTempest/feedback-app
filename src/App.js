8b76f66d2d9c01a50f196a509596dbde81cd93f1
Blake <zabulonov.a@gmail.com>
2022-02-04 23:17:57 +0200

    Font Awesome error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import { useState } from 'react';
 
 function App() {
+	const [feedback, setFeedback] = useState(FeedbackData);
 	return (
 		<>
 			<Header />
 			<div className="container">
-				<h1>My App</h1>
+				<FeedbackList feedback={feedback} />
 			</div>
 		</>
 	);
